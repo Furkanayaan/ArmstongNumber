9fa9bff95d2b0fd3d464f752b0b7ce5dd0bfaf9f
Furkanayaan <ayanf16@itu.edu.tr>
2022-09-12 17:09:38 +0300

    ArmstongNumber

---
+++ ArmstongNumber/ArmstongNumber/Class1.cs
@@ -0,0 +1,32 @@
+﻿using  System;
+namespace ArmstongNumber
+
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            int a = 0;
+            double sonuc = 0;
+            for (int j = 0; j < 10000; j++)
+            {
+                
+                for (int i = 0; i < j.ToString().Length; i++)
+                {
+                    int b = Convert.ToInt32(j % Math.Pow(10, i + 1));
+                    b = b / Convert.ToInt32(Math.Pow(10, i));
+                    sonuc += Math.Pow(b, 3);
+                    b = 0;
+                }
+
+                if (sonuc == j)
+                {
+                    Console.WriteLine(j + "" +  "" + " Bu sayı Armstong sayısıdır");
+                }
+
+                sonuc = 0;
+            }
+            
+        } 
+    }
+}
